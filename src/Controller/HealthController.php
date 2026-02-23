029e97341b14d4afe91074f3359a2aa51e4e6398
zigomario <zigomario@free.fr>
2026-02-23 11:07:13 +0100

    fast test api

---
+++ src/Controller/HealthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HealthController
+{
+    #[Route('/api/health', name: 'api_health', methods: ['GET'])]
+    public function __invoke(): JsonResponse
+    {
+        return new JsonResponse([
+            'status' => 'ok',
+        ]);
+    }
+}
