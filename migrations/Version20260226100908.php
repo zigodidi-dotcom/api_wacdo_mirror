2e34c6b45042966b09fae64231b4578cfbda0a6e
zigomario <zigomario@free.fr>
2026-02-26 15:48:44 +0100

    change migration dateembuache 3

--- migrations/Version20260226100908.php
+++ migrations/Version20260226100908.php
@@ -19,11 +19,11 @@ final class Version20260226100908 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE collaborateur CHANGE dateembauche date_embauche DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE collaborateur CHANGE date_embauche dateembauche DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE collaborateur CHANGE date_embauche dateembauche DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE collaborateur CHANGE dateembauche date_embauche DATETIME DEFAULT NULL');
     }
 }
