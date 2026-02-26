de793dda03adcb5a6a1f2a9fa82a75e0930e7786
zigomario <zigomario@free.fr>
2026-02-26 16:45:36 +0100

    create admin command 2

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -5,7 +5,10 @@ echo "Running migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
 if [ "${CREATE_ADMIN:-0}" = "1" ]; then
+      echo "[createadmin] Loads..."
   php bin/console app:create-admin "${ADMIN_EMAIL:?missing}" "${ADMIN_PASSWORD:?missing}" "${ADMIN_PRENOM:-Admin}" "${ADMIN_NOM:-User}"
+else
+    echo "[createadmin] Skipping loads"
 fi
 
 echo "Starting app on port ${PORT:-8080}..."
